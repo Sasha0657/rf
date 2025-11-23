11de9597c69e878c4ac5aae501641009c471a32c
Sasha0657 <s211489@mail.ru>
2025-11-23 19:11:24 +0300

    Add files via upload

--- lightbox.js
+++ lightbox.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const images = document.querySelectorAll('.gallery img');
-  const lightbox = document.createElement('div');
-  lightbox.classList.add('lightbox');
-  document.body.appendChild(lightbox);
+  document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery img');
+    const lightbox = document.createElement('div');
+    lightbox.classList.add('lightbox');
+    document.body.appendChild(lightbox);
 
-  const img = document.createElement('img');
-  lightbox.appendChild(img);
+    const img = document.createElement('img');
+    lightbox.appendChild(img);
 
-  images.forEach(image => {
-    image.addEventListener('click', () => {
-      img.src = image.src;
-      lightbox.style.display = 'flex';
+    images.forEach(image => {
+      image.addEventListener('click', () => {
+        img.src = image.src;
+        lightbox.style.display = 'flex';
+      });
+    });
+
+    lightbox.addEventListener('click', () => {
+      lightbox.style.display = 'none';
     });
   });
-
-  lightbox.addEventListener('click', () => {
-    lightbox.style.display = 'none';
-  });
-});
