d32dc9f233ef6228e7cab2153917315298890e02
Sasha0657 <s211489@mail.ru>
2026-01-15 15:05:49 +0300

    Delete fallback-sw.js

--- fallback-sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      // Если запрос НЕ УДАЛСЯ — редиректим на GitHub Pages
-      return Response.redirect('https://sasha0657.github.io/rf/index.html', 302);
-    })
-  );
-
-});
